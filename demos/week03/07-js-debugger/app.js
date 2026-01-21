3c171e74b1e363568ac3f5344ebf701cc0b6d143
Sam Louloudis <samanthaleelouloudis@gmail.com>
2026-01-21 09:29:09 +0000

    debugger

---
+++ demos/week03/07-js-debugger/app.js
@@ -0,0 +1,20 @@
+console.log('Are you working...')
+
+const myFavNumber = 45;
+
+function countToTen() {
+    sum(15,14)
+    for (let i = 0; i <= 10; i++) {
+        console.log(i)
+    }
+}
+
+countToTen()
+
+
+
+function sum(a,b) {
+    // we can use the debugger statement to pause execution when we're in the sources tab
+    debugger;
+    return a + b
+}
