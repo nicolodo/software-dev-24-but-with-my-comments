f5f5f1e08a07c7472143151042d4b747118805e2
Sam Louloudis <samanthaleelouloudis@gmail.com>
2026-01-30 11:54:26 +0000

    add demo to class repo

---
+++ demos/week04/08-assingment-demo/sql.sql
@@ -0,0 +1,8 @@
+-- copy this into SUPABASE!!!
+CREATE TABLE IF NOT EXISTS messages (
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    msg_name TEXT,
+    content TEXT
+);
+
+INSERT INTO messages (msg_name, content) VALUES ('hello there','general kenobi');
