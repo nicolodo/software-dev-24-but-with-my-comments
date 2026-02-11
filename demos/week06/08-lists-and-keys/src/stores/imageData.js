da19fcd2fdaedeb63ac90517d99387ed21401bf2
Sam Louloudis <samanthaleelouloudis@gmail.com>
2026-02-11 10:26:16 +0000

    hello

---
+++ demos/week06/08-lists-and-keys/src/stores/imageData.js
@@ -0,0 +1,32 @@
+export const images = [
+    {
+      id: 1,
+      imgUrl: "https://images.unsplash.com/photo-1554629947-334ff61d85dc",
+      title: "Grayscale Photography of Mountain",
+      author: "Daniel Leone",
+    },
+    {
+      id: 2,
+      imgUrl: "https://images.unsplash.com/photo-1465146344425-f00d5f5c8f07",
+      title: "Orange Flowers",
+      author: "Henry Be",
+    },
+    {
+      id: 3,
+      imgUrl: "https://images.unsplash.com/photo-1448375240586-882707db888b",
+      title: "Pathway Between Green Trees",
+      author: "Jay Mantri",
+    },
+    {
+      id: 4,
+      imgUrl: "https://images.unsplash.com/photo-1533038590840-1cde6e668a91",
+      title: "Pink Petaled Flower",
+      author: "Annie Spratt",
+    },
+    {
+      id: 5,
+      imgUrl: "https://images.unsplash.com/photo-1528164344705-47542687000d",
+      title: "Low Angle Photo of Building",
+      author: "Maxime Lebrun",
+    },
+  ];
