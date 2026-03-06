e9ab3b7cde27b1a2c57f39228b39dada31a6abbd
Sam Louloudis <samanthaleelouloudis@gmail.com>
2026-03-06 09:29:25 +0000

    intro to task

---
+++ demos/week09/13-comments-form-demo/src/components/AddComment.jsx
@@ -0,0 +1,1 @@
+// a form to add new comments
---
+++ demos/week09/13-comments-form-demo/src/components/DeleteButton.jsx
@@ -0,0 +1,8 @@
+'use client'
+// on button click.
+// if I want to respond to a user input and run something?
+export default function DeleteButton() {
+    return (
+        <button onClick={() => {}}>Delete</button>
+    )
+}
