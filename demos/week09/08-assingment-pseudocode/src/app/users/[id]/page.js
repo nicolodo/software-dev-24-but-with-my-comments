80e9d2fb52e43500ff1d407576eb5ec6bc51ebff
Sam Louloudis <samanthaleelouloudis@gmail.com>
2026-03-11 10:12:49 +0000

    user details

--- demos/week09/08-assingment-pseudocode/src/app/users/[id]/page.js
+++ demos/week09/08-assingment-pseudocode/src/app/users/[id]/page.js
@@ -1,11 +1,18 @@
 import { getUser } from "@/utils/getUser"
 
 export default async function UserPage() {
+
+    // this will always read the currently looged in user
+    // could we read from params to get other user profiles? 
     const user = await getUser()
 
+    console.log(user[0])
     return (
         <div>
             <p>This is the user page</p>
+            <p>{user[0].username}</p>
+            <p>bio: </p>
+            <p>{user[0].bio}</p>
         </div>
     )
 }
