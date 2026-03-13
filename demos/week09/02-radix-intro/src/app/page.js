9ace2a80507a332173b58c25be81565c16f2f52e
nicolas <nicolasbernard19@gmail.com>
2026-03-13 13:27:33 +0000

    import and use an icon on the pg

--- demos/week09/02-radix-intro/src/app/page.js
+++ demos/week09/02-radix-intro/src/app/page.js
@@ -1,10 +1,13 @@
 import NavBar from "@/components/NavigationMenu";
 import PopoverExample from "@/components/Popover";
 
+//primitive radix Components I'm trying out from the docs
 import NBlockquote from "@/components/NBlockquote";
 import NCard from "@/components/NCard";
 import NSwitch from "@/components/NSwitch";
 import NPopover2 from "@/components/NPopover2/NPopover2";
+
+import { LuAlarmClockCheck } from "react-icons/lu"; // icon
 
 export default function Page() {
   return (
@@ -17,6 +20,7 @@ export default function Page() {
         <NCard /> {/* Card not appearing as on site */}
         <NSwitch />
         <NPopover2 />
+        <LuAlarmClockCheck /> {/* puts a little alarm clock icon on the pg */}
       </div>
     </div>
   )
